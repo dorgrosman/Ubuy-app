ec28fbadf7df700534ee79f248c3415fa44e32e0
dorgrosman <ddorgrosman@gmail.com>
2021-02-12 19:20:14 +0200

    fiix address shiping bug

--- frontend/src/pages/ShippingAddressPage/ShippingAddressPage.jsx
+++ frontend/src/pages/ShippingAddressPage/ShippingAddressPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 import CheckoutSteps from '../../cmps/CheckoutSteps'
 import { useDispatch, useSelector } from 'react-redux';
 import { saveShippingAddress } from '../../actions/CartActions';
