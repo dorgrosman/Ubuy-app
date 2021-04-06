086b12fe249519e290f83dcd1229cc3d53c3ad5a
dorgrosman <ddorgrosman@gmail.com>
2021-04-06 17:41:57 +0300

    s

--- frontend/src/cmps/Product/Product.jsx
+++ frontend/src/cmps/Product/Product.jsx
@@ -10,23 +10,24 @@ const Product = (props) => {
     console.log('product:', product)
 
     return (
-        <div key={product._id} className="card" >
-            <Link to={`/product/${product._id}`} ><img className="medium" src={product.img} alt="product" /></Link>
-            <div className="card-body">
-
-                <Link to={`/product/${product._id}`}><p className="title" to={`/product/${product._id}`} >{product.name}</p><Link to={`/product/${product._id}`}><p className="desc" to={`/product/${product._id}`} >{product.description}</p></Link></Link>
-                
-                <div className="row">Price : <h3 className="price"> {product.price} $</h3></div>
-                {/* <Rating
+        <Link to={`/product/${product._id}`} >
+            <div key={product._id} className="card" >
+                <div className="card-body">
+                    <Link to={`/product/${product._id}`} ><img className="medium" src={product.img} alt="product" /></Link>
+                    <Link to={`/product/${product._id}`}><p className="title" to={`/product/${product._id}`} >{product.name}</p><Link to={`/product/${product._id}`}><p className="desc" to={`/product/${product._id}`} >{product.description}</p></Link></Link>
+                    {/* <hr></hr>   */}
+                    <div className="row">Price : <h3 className="price"> {product.price} $</h3></div>
+                    {/* <Rating
                     rating={product.rating}
                     numReviews={product.numReviews} /> */}
-                {/* <div className="row">
+                    {/* <div className="row">
                     <div>
                     </div>
                 </div> */}
 
+                </div>
             </div>
-        </div>
+        </Link>
     )
 
 }
